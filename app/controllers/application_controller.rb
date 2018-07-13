0cb436c67a2eb3295be820dfd7ca36b3fdbf822d
iamdeveloper-me <iamdeveloper.me@gmail.com>
2018-07-13 13:12:38 +0530

    Added Devise gem & enable sign-in, sign-out, update-profile.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    additional_params = [:first_name, :last_name]
+    
+    devise_parameter_sanitizer.permit(:sign_up, keys: additional_params)
+    devise_parameter_sanitizer.permit(:account_update,keys: additional_params)
+  end
 end
