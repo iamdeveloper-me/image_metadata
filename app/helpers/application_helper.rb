2884bd78074caaba63d6b654bd81f723151922a2
iamdeveloper-me <iamdeveloper.me@gmail.com>
2018-07-30 18:23:29 +0530

    Design changes.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def active_tab(tab_name = nil)
+    if controller_name == 'sessions' && action_name == 'new' && tab_name == "login"
+      "active"
+    elsif controller_name == 'registrations' && action_name == 'edit' && tab_name == "upate_profile"
+      "active"
+    elsif controller_name == 'photos' && action_name == 'new' && tab_name == "new_photo"
+      "active"
+    elsif controller_name == 'photos' && action_name == 'index' && tab_name == "index_photo"
+      "active"
+    elsif controller_name == 'home' && action_name == 'index' && tab_name == "home"
+      "active"
+    end
+  end
 end
